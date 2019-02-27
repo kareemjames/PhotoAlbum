6bb97b9208c55bf65a6d6a062d1c5d01c55f6d53
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-02-26 22:03:44 -0500

    initial commit, crate photo model

---
+++ src/main/java/com/detroitlabs/photoalbum/model/Photo.java
@@ -0,0 +1,51 @@
+package com.detroitlabs.photoalbum.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Photo {
+
+    private String name;
+    private LocalDate dateUploaded;
+    private List<String> tags;
+
+    public Photo(String name, LocalDate dateUploaded) {
+        this.name = name;
+        this.dateUploaded = dateUploaded;
+        tags = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "name='" + name + '\'' +
+                ", dateUploaded=" + dateUploaded +
+                ", tags=" + tags +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateUploaded() {
+        return dateUploaded;
+    }
+
+    public void setDateUploaded(LocalDate dateUploaded) {
+        this.dateUploaded = dateUploaded;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
