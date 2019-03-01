4ba3548f512908a1df9a5d743d71472c6156ed0a
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-02-28 22:54:20 -0500

    change updload date values, test methods for name and date sort via buttons successfully, create mapping for new pages

--- src/main/java/com/detroitlabs/photoalbum/controller/PhotoController.java
+++ src/main/java/com/detroitlabs/photoalbum/controller/PhotoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
 @Controller
@@ -19,6 +20,22 @@ public class PhotoController {
     @RequestMapping("/")
     public String displayAllPhotos(ModelMap modelMap) {
         List<Photo> allPhotos = photoRepository.getAllPhotos();
+
+        modelMap.put("allPhotos", allPhotos);
+        return "home";
+    }
+
+    @RequestMapping("/name")
+    public String displayAllPhotosSortedByName(ModelMap modelMap) {
+        List<Photo> allPhotos = photoRepository.getPhotosAlphabeticallyByName();
+
+        modelMap.put("allPhotos", allPhotos);
+        return "home";
+    }
+
+    @RequestMapping("/date")
+    public String displayAllPhotosSortedByUploadDate(ModelMap modelMap) {
+        List<Photo> allPhotos = photoRepository.getPhotosByDateUploaded();
 
         modelMap.put("allPhotos", allPhotos);
         return "home";
--- src/main/java/com/detroitlabs/photoalbum/data/PhotoRepository.java
+++ src/main/java/com/detroitlabs/photoalbum/data/PhotoRepository.java
@@ -11,16 +11,16 @@ public class PhotoRepository {
 
     //TODO add arraylist of tags to constructor so I can create method to search by tags
     private static final List<Photo> ALL_PHOTOS = Arrays.asList(
-            new Photo("MazdaRx7", LocalDate.of(2019, 2, 26), "yellowrx7.jpg", "A mini description"),
-            new Photo("ToyotaSupra", LocalDate.of(2019, 2, 26), "blackmk4supra.jpg", "A mini description"),
-            new Photo("HondaCivic", LocalDate.of(2019, 2, 26), "egcivichatch.jpg", "A mini description"),
-            new Photo("AcuraIntegra", LocalDate.of(2019, 2, 26), "whitedc2typer.jpg", "A mini description"),
-            new Photo("SubaruWrxSti", LocalDate.of(2019, 2, 26), "bluewrxsti.jpg", "A mini description"),
-            new Photo("AcuraNsx", LocalDate.of(2019, 2, 26), "whitensxtyper.jpg", "A mini description"),
-            new Photo("MitsubishiLancerEvo", LocalDate.of(2019, 2, 26), "blacklancerevo.jpg", "A mini description"),
-            new Photo("ToyotaMr2", LocalDate.of(2019, 2, 26), "greenmr2.jpg", "A mini description"),
-            new Photo("HondaCrx", LocalDate.of(2019, 2, 26), "greencrx.jpg", "A mini description"),
-            new Photo("NissanSkylineGtr", LocalDate.of(2019, 2, 26), "bluegtr.jpg", "A mini description")
+            new Photo("MazdaRx7", LocalDate.of(2019, 1, 15), "yellowrx7.jpg", "A mini description"),
+            new Photo("ToyotaSupra", LocalDate.of(2019, 1, 13), "blackmk4supra.jpg", "A mini description"),
+            new Photo("HondaCivic", LocalDate.of(2019, 2, 2), "egcivichatch.jpg", "A mini description"),
+            new Photo("AcuraIntegra", LocalDate.of(2019, 2, 18), "whitedc2typer.jpg", "A mini description"),
+            new Photo("SubaruWrxSti", LocalDate.of(2018, 10, 4), "bluewrxsti.jpg", "A mini description"),
+            new Photo("AcuraNsx", LocalDate.of(2018, 4, 29), "whitensxtyper.jpg", "A mini description"),
+            new Photo("MitsubishiLancerEvo", LocalDate.of(2017, 6, 15), "blacklancerevo.jpg", "A mini description"),
+            new Photo("ToyotaMr2", LocalDate.of(2017, 8, 9), "greenmr2.jpg", "A mini description"),
+            new Photo("HondaCrx", LocalDate.of(2017, 3, 10), "greencrx.jpg", "A mini description"),
+            new Photo("NissanSkylineGtr", LocalDate.of(2017, 12, 25), "bluegtr.jpg", "A mini description")
     );
 
     public List<Photo> getAllPhotos() {
@@ -28,9 +28,9 @@ public class PhotoRepository {
     }
 
     public List<Photo> getPhotosAlphabeticallyByName() {
-
         List<Photo> sortedPhotosByName = new ArrayList<>();
         sortedPhotosByName.addAll(getAllPhotos());
+
         Comparator.comparing(Photo::getName).thenComparing(Photo::getName);
         Collections.sort(sortedPhotosByName, Comparator.comparing(Photo::getName));
         return sortedPhotosByName;
@@ -39,6 +39,7 @@ public class PhotoRepository {
     public List<Photo> getPhotosByDateUploaded() {
         List<Photo> sortedPhotosByDate = new ArrayList<>();
         sortedPhotosByDate.addAll(getAllPhotos());
+
         Comparator.comparing(Photo::getDateUploaded).thenComparing(Photo::getDateUploaded);
         Collections.sort(sortedPhotosByDate, Comparator.comparing(Photo::getDateUploaded));
         return sortedPhotosByDate;
